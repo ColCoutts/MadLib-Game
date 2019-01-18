87a4ffbcf4f2c31650c87bf87eb083d856667f4a
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-01-17 21:10:23 -0800

    created input values or at least tried...

--- main.js
+++ main.js
@@ -1,17 +1,11 @@
-//var first input
-var firstNounInput = document.getElementById('noun-1');
-
-//var second input
+//var input
+var firstNounInput = document.getElementById('noun-one');
 var adjectiveInput = document.getElementById('adjective');
-
-//var third input
-var secondNounInput = document.getElementById('noun-2');
-
-//var fourth input
+var secondNounInput = document.getElementById('noun-two');
 var verbInput = document.getElementById('verb');
-
-//var fifth input
 var bodyPartInput = document.getElementById('body-part');
-
-//var sixth input
 var adverbInput = document.getElementById('adverb');
+
+//get value out of input
+
+var noun-one = firstNounInput.value;
