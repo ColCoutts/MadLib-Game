ea5b3fea7c682f3b070a7117b3345792b0cedc1d
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-01-17 21:05:28 -0800

    created text and input boxes, created id and js variables

---
+++ main.js
@@ -0,0 +1,17 @@
+//var first input
+var firstNounInput = document.getElementById('noun-1');
+
+//var second input
+var adjectiveInput = document.getElementById('adjective');
+
+//var third input
+var secondNounInput = document.getElementById('noun-2');
+
+//var fourth input
+var verbInput = document.getElementById('verb');
+
+//var fifth input
+var bodyPartInput = document.getElementById('body-part');
+
+//var sixth input
+var adverbInput = document.getElementById('adverb');
