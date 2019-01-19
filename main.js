c6eac0a695fc778caf311d869456eaa8adbff0fb
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-01-18 16:52:58 -0800

    Structure of madlib now in place resolving appearance of final madlib

--- main.js
+++ main.js
@@ -22,5 +22,14 @@ function madliberize(){
     var adverbOne = adverbInput.value;
     console.log (adverbInput.value)
 
+// make greeting
+var greeter = 'Please add one ' + nounOne + ' to your' + adjOne + ' stew. Stir' + verbOne + ' with your ' + nounTwo + '. Season with' + bodyBit + ', then enjoy' + adverbOne;
+
+// display greeting
+var hello = document.getElementById('hello');
+hello.textContent = greeter;
+
+//remove prompt
+
 }
 
