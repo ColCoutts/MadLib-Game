28945989c02ba6450151dcff36394d4d7d2df6bf
Colin Coutts <couttstcolin@gmail.com>
2019-01-21 12:43:22 -0800

    try addEventListener to alter input values when clicked...still no go

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 function madliberize(){
 
     //get inputs by id
-    var firstNounInput = document.getElementById('noun-one');
+    var firstNounInput = document.getElementById('noun-one')
     var adjectiveInput = document.getElementById('adjective');
     var verbInput = document.getElementById('verb');
     var secondNounInput = document.getElementById('noun-two');
@@ -21,17 +21,19 @@ function madliberize(){
     
     var adverbOne = adverbInput.value;
     console.log (adverbInput.value)
+    
+    // make greeting
+    var greeter = 'Please add one '+ nounOne + ' to your ' + adjOne + ' stew. Stir ' + verbOne + ' with your ' + nounTwo + '. Season with ' + bodyBit + ', then enjoy ' + adverbOne+ '!';
+    
+    // display greeting
+    
+    var hello = document.getElementById('hello');
+    hello.textContent = greeter;
+    
+    const button = document.querySelector('red');
 
-// make greeting
-var greeter = 'Please add one ' + nounOne + ' to your ' + adjOne + ' stew. Stir ' + verbOne + ' with your ' + nounTwo + '. Season with ' + bodyBit + ', then enjoy ' + adverbOne+ '!';
-
-// display greeting
-
-
-var hello = document.getElementById('hello');
-hello.textContent = greeter;
-
-//remove prompt
-
+    button.addEventListener('click', event => {
+        button.innerHTML = 'Click count: ${event.detail}';
+    });
 }
 
