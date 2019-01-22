a546edbd7c5a16d2301965811cc79f3eaecd0f7a
Colin Coutts <couttstcolin@gmail.com>
2019-01-21 16:17:57 -0800

    changed output text to different color, client output not differentiated

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function madliberize(){
 
     //get inputs by id
     var firstNounInput = document.getElementById('noun-one')
+    //adding "red" color style here produces red in input box
     var adjectiveInput = document.getElementById('adjective');
     var verbInput = document.getElementById('verb');
     var secondNounInput = document.getElementById('noun-two');
@@ -22,18 +23,16 @@ function madliberize(){
     var adverbOne = adverbInput.value;
     console.log (adverbInput.value)
     
+    document.getElementById('hello').style.color = "blue";
+    
     // make greeting
     var greeter = 'Please add one '+ nounOne + ' to your ' + adjOne + ' stew. Stir ' + verbOne + ' with your ' + nounTwo + '. Season with ' + bodyBit + ', then enjoy ' + adverbOne+ '!';
-    
+
     // display greeting
     
     var hello = document.getElementById('hello');
     hello.textContent = greeter;
     
-    const button = document.querySelector('red');
-
-    button.addEventListener('click', event => {
-        button.innerHTML = 'Click count: ${event.detail}';
-    });
+    
 }
 
